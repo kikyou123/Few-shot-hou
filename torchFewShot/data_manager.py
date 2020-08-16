d9e91c3dc6d683b3afe511b2143173d3ff0a4c4d
f-GAN <1163763177@qq.com>
2020-08-16 21:01:15 +0800

    Add files via upload

---
+++ torchFewShot/data_manager.py
@@ -0,0 +1,103 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+from torch.utils.data import DataLoader
+
+import transforms as T
+import datasets
+import dataset_loader
+
+class DataManager(object):
+    """
+    Few shot data manager
+    """
+
+    def __init__(self, args, use_gpu):
+        super(DataManager, self).__init__()
+        self.args = args
+        self.use_gpu = use_gpu
+
+        print("Initializing dataset {}".format(args.dataset))
+        dataset = datasets.init_imgfewshot_dataset(name=args.dataset, k=args.k)
+
+        if args.load:
+            transform_train = [
+                T.RandomCrop(84, padding=8),
+                #T.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+                T.RandomHorizontalFlip(),
+                T.ToTensor(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+
+            transform_test = T.Compose([
+                T.ToTensor(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+
+        else:
+            transform_train = [
+                T.Resize((args.height, args.width), interpolation=3),
+                T.RandomCrop(args.height, padding=8),
+                T.RandomHorizontalFlip(),
+                T.ToTensor(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+
+            transform_test = T.Compose([
+                T.Resize((args.height, args.width), interpolation=3),
+                T.ToTensor(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+
+        if args.erasing_p > 0:
+            transform_train += [T.RandomErasing(args.erasing_p)]
+        transform_train = T.Compose(transform_train)
+
+        pin_memory = True if use_gpu else False
+
+        self.trainloader = DataLoader(
+                dataset_loader.init_loader(name='cls_loader',
+                    dataset=dataset.train,
+                    transform=transform_train,
+                    load=args.load,
+                ),
+                batch_size=args.train_batch, shuffle=True, num_workers=args.workers,
+                pin_memory=pin_memory, drop_last=True,
+        )
+
+        self.valloader = DataLoader(
+                dataset_loader.init_loader(name='test_loader',
+                    dataset=dataset.val,
+                    labels2inds=dataset.val_labels2inds,
+                    labelIds=dataset.val_labelIds,
+                    n_way=args.n_way,
+                    n_shot=args.n_shot,
+                    query_shot=args.query_shot,
+                    epoch_size=args.epoch_size,
+                    transform=transform_test,
+                    load=args.load,
+                ),
+                batch_size=args.test_batch, shuffle=False, num_workers=args.workers,
+                pin_memory=pin_memory, drop_last=False,
+        )
+        self.testloader = DataLoader(
+                dataset_loader.init_loader(name='test_loader',
+                    dataset=dataset.test,
+                    labels2inds=dataset.test_labels2inds,
+                    labelIds=dataset.test_labelIds,
+                    n_way=args.n_way,
+                    n_shot=args.n_shot,
+                    query_shot=args.query_shot,
+                    epoch_size=args.epoch_size,
+                    transform=transform_test,
+                    load=args.load,
+                ),
+                batch_size=args.test_batch, shuffle=False, num_workers=args.workers,
+                pin_memory=pin_memory, drop_last=False,
+        )
+
+    def return_dataloaders(self):
+        if self.args.phase == 'test':
+            return self.trainloader, self.testloader
+        elif self.args.phase == 'val':
+            return self.trainloader, self.valloader
---
+++ torchFewShot/losses.py
@@ -0,0 +1,99 @@
+from __future__ import absolute_import
+from __future__ import division
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CrossEntropyLoss(nn.Module):
+    """Cross entropy loss with label smoothing regularizer.
+    
+    Reference:
+    Szegedy et al. Rethinking the Inception Architecture for Computer Vision. CVPR 2016.
+    
+    Equation: y = (1 - epsilon) * y + epsilon / K.
+    
+    Args:
+    - num_classes (int): number of classes
+    - epsilon (float): weight
+    - use_gpu (bool): whether to use gpu devices
+    - label_smooth (bool): whether to apply label smoothing, if False, epsilon = 0
+    """
+    def __init__(self, num_classes, epsilon, use_gpu=True):
+        super(CrossEntropyLoss, self).__init__()
+        print('epsilon: {}'.format(epsilon))
+        self.num_classes = num_classes
+        self.epsilon = epsilon 
+        self.use_gpu = use_gpu
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, inputs, targets):
+        """
+        Args:
+        - inputs: prediction matrix (before softmax) with shape (batch_size, num_classes)
+        - targets: ground truth labels with shape (num_classes)
+        """
+        log_probs = self.logsoftmax(inputs)
+        targets = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1)
+        if self.use_gpu: targets = targets.cuda()
+        targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
+        loss = (- targets * log_probs).mean(0).sum()
+        return loss
+
+
+
+class TripletLoss(nn.Module):
+    """Triplet loss with hard positive/negative mining.
+
+    Reference:
+    Hermans et al. In Defense of the Triplet Loss for Person Re-Identification. arXiv:1703.07737.
+
+    Code imported from https://github.com/Cysu/open-reid/blob/master/reid/loss/triplet.py.
+
+    Args:
+    - margin (float): margin for triplet.
+    """
+    def __init__(self, margin=0.3, distance='euclidean', use_gpu=True):
+        super(TripletLoss, self).__init__()
+        if distance not in ['euclidean', 'consine']:
+            raise KeyError('Unsupported distance: {}'.format(distance))
+        self.distance = distance
+        self.margin = margin
+        self.use_gpu = use_gpu
+        self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+
+    def forward(self, inputs, targets):
+        """
+        Args:
+        - inputs: feature matrix with shape (batch_size, feat_dim)
+        - targets: ground truth labels with shape (num_classes)
+        """
+        n = inputs.size(0)
+        
+        # Compute pairwise distance, replace by the official when merged
+        if self.distance == 'euclidean':
+            dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)
+            dist = dist + dist.t()
+            dist.addmm_(1, -2, inputs, inputs.t())
+            dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
+        elif self.distance == 'consine':
+            fnorm = torch.norm(inputs, p=2, dim=1, keepdim=True)
+            l2norm = inputs.div(fnorm.expand_as(inputs))
+            dist = - torch.mm(l2norm, l2norm.t())
+
+        if self.use_gpu: targets = targets.cuda()
+        
+        # For each anchor, find the hardest positive and negative
+        mask = targets.expand(n, n).eq(targets.expand(n, n).t())
+        dist_ap, dist_an = [], []
+        for i in range(n):
+            dist_ap.append(dist[i][mask[i]].max().unsqueeze(0))
+            dist_an.append(dist[i][mask[i] == 0].min().unsqueeze(0))
+        dist_ap = torch.cat(dist_ap)
+        dist_an = torch.cat(dist_an)
+        
+        # Compute ranking hinge loss
+        y = torch.ones_like(dist_an)
+        loss = self.ranking_loss(dist_an, dist_ap, y)
+        return loss
