21fe2807e126498b39f8498b204fdf6c0b886c00
f-GAN <1163763177@qq.com>
2020-08-16 21:07:40 +0800

    Update __init__.py

--- torchFewShot/models/__init__.py
+++ torchFewShot/models/__init__.py
@@ -1,1 +1,30 @@
 
+from __future__ import absolute_import
+
+from .net import ResNet12, AngleLinear, Classifier
+from .mlp import MLPN
+
+from .protohead import ProtoNetHead
+
+
+__model_factory = {
+        'ResNet12': ResNet12,
+        'AngleLinear': AngleLinear,
+        'Classifier': Classifier,
+
+        'Proto': ProtoNetHead,
+
+        'MLP': MLPN,
+}
+
+
+
+def get_names():
+    return list(__model_factory.keys()) 
+
+
+def init_model(name, *args, **kwargs):
+    if name not in list(__model_factory.keys()):
+        raise KeyError("Unknown model: {}".format(name))
+    return __model_factory[name](*args, **kwargs)
+
